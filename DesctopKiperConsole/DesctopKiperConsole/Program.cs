ad69192bb8adbe91bda9e14760a9aeba57356a1b
cibergod <cibergod1@gmail.com>
2015-07-27 17:48:40 +0300

    Пока консольный проект

---
+++ DesctopKiperConsole/DesctopKiperConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesctopKiperConsole
+{
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
